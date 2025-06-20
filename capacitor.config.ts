5ff5459472f953c4270276361caeca884ce47c74
Adilio Ismael dos Santos <AdilioFkis@gmail.com>
2025-06-20 19:48:24 +0000

    Deu esse erro agora: PS C:\Users\admda\StudioProjects\Controle-de-doces_

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.adesmael.controledoces',
+  appName: 'Controle de Doces',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
